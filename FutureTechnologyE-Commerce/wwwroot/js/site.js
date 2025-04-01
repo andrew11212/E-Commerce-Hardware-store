9f3c50fba90faa046caade217e2f6f430a95af31
Mario14802 <mario.atef720@gmail.com>
2025-04-02 00:27:56 +0200

    add storage accesories titles and EGP

--- FutureTechnologyE-Commerce/wwwroot/js/site.js
+++ FutureTechnologyE-Commerce/wwwroot/js/site.js
@@ -1,4 +1,12 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
-
+document.addEventListener('DOMContentLoaded', function () {
+    // Language direction handling
+    const htmlElement = document.documentElement;
+    if (htmlElement.lang.startsWith('ar')) {
+        htmlElement.dir = 'rtl';
+    } else {
+        htmlElement.dir = 'ltr';
+    }
+});
 // Write your JavaScript code.
