cb53cb79711f042cd5f3497299c8f2f47cdc6e67
Andrew Atef Nasr <134229268+andrew11212@users.noreply.github.com>
2025-04-01 21:36:05 +0200

    add search

--- FutureTechnologyE-Commerce/Models/Brand.cs
+++ FutureTechnologyE-Commerce/Models/Brand.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace FutureTechnologyE_Commerce.Models
 {
@@ -10,7 +11,7 @@ namespace FutureTechnologyE_Commerce.Models
 		[Required]
 		[StringLength(100)]
 		public string Name { get; set; }
-
+		[ValidateNever]
 		public virtual ICollection<Product> Products { get; set; }
 	}
 }
