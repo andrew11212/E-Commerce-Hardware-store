ac4db7917fcd23b0dfdc6750411d7c41dc18524d
Andrew Atef Nasr <134229268+andrew11212@users.noreply.github.com>
2025-04-08 17:34:00 +0200

    ui

--- FutureTechnologyE-Commerce/Models/ViewModels/HomeIndexViewModel.cs
+++ FutureTechnologyE-Commerce/Models/ViewModels/HomeIndexViewModel.cs
@@ -3,7 +3,7 @@
 	public class HomeIndexViewModel
 	{
 		public int PageNumber { get; set; } = 1;
-		public int PageSize { get; set; } = 4;
+		public int PageSize { get; set; } = 9;
 		public int TotalCount { get; set; }
 		public string SearchString { get; set; } = string.Empty;
 		public IEnumerable<Product>? Products { get; set; }
